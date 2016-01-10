46fcc7a690a7e7281f4990a0ea90da068e9fad9b
pablodarocha <pablodarocha.ribeiro@gmail.com>
2016-01-10 16:30:21 -0200

    + bower and gulp done

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
 
+var paths = {
+  'sass': './resources/assets/sass/'
+}
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,5 +16,24 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix
+    .copy('public/libs_bower/semantic/dist/semantic.min.js', 'public/assets/js/semantic.min.js')
+    .copy('public/libs_bower/semantic/dist/semantic.min.css', 'public/assets/css/semantic.min.css')
+    .copy('public/libs_bower/jquery/dist/jquery.min.js', 'public/assets/js/jquery.min.js')
+    .sass([
+      'app.scss',
+    ], 'public/assets/css'), {includePaths: [paths.sass] }
+    //.version([
+    //  'public/assets/css/main.css',
+    //  'public/assets/js/semantic.js',
+    //  'public/assets/css/semantic.css',
+    //])
+  ;
+});
+
+gulp.task('fonts', function() {
+  gulp.src(['public/libs_bower/semantic-ui/dist/themes/default/assets/fonts/**/*'])
+      .pipe(gulp.dest('public/assets/fonts'));
+  //gulp.src(['bower_components/fontawesome/fonts/**/*'])
+  //    .pipe(gulp.dest('public/build/assets/css/fonts'));
 });
